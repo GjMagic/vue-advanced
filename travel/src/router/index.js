2a12e62da85fa125bbc5ea6dbd74d7a620470db0
GjMagic <862635037@qq.com>
2018-04-23 11:55:30 +0800

    DetailBanner组件开发

--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home/Home'
 import City from '@/views/City/City'
+import Detail from '@/views/Detail/Detail'
 
 Vue.use(Router)
 
@@ -14,5 +15,9 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
-    },]
+    }, {
+      path: '/detail/:id', // :参数(动态路由) 注: 注意冒号前加/
+      name: 'Detail',
+      component: Detail
+    }]
 })
---
+++ travel/src/views/Detail/components/DetailBanner.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="banner">
+    <img 
+      class="banner-img"
+      src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" 
+    />
+    <div class="banner-info">
+      <div class="banner-title">
+        故宫(AAAAA景区)
+      </div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe692;</span>
+        39
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .8rem
+      color #ffffff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8)) 
+      .banner-title
+        font-size .426667rem
+        flex 1
+        padding 0 .266667rem
+      .banner-number
+        height .426667rem
+        line-height .426667rem
+        margin-top .186667rem
+        padding 0 .426667rem
+        border-radius .266667rem
+        background-color rgba(0, 0, 0, .8)
+        .banner-icon
+          font-size .32rem
+</style>
