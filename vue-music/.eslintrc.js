39eda353f4d55aa608f16e1e998dfceeb198a2b2
GjMagic <862635037@qq.com>
2018-06-03 18:19:06 +0800

    项目目录初始化

--- vue-music/.eslintrc.js
+++ vue-music/.eslintrc.js
@@ -24,6 +24,10 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    'eol-last': 0,
+    'space-before-function-paren': 0
   }
 }
--- vue-music/build/webpack.base.conf.js
+++ vue-music/build/webpack.base.conf.js
@@ -35,6 +35,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       '@': resolve('src'),
+      'assets': resolve('src/assets')
     }
   },
   module: {
