853447521657ad5d0ab39e85438c910c4a3e440d
GjMagic <862635037@qq.com>
2018-07-01 19:14:24 +0800

    封装jsonp

--- vue-music/src/assets/js/jsonp.js
+++ vue-music/src/assets/js/jsonp.js
@@ -1,27 +1,25 @@
-/**
- * Created by Administrator on 2017/7/13 0013.
- */
 import originJSONP from 'jsonp'
 
-export default function jsonp(url, data, option) {
-  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+export default function jsonp (url, data, opts) {
+
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
 
   return new Promise((resolve, reject) => {
-    originJSONP(url, option, (err, data) => {
+    originJSONP(url, opts, (err, data) => {
       if (!err) {
-        resolve(data)
+        resolve(data);
       } else {
-        reject(err)
+        reject(err);
       }
     })
   })
 }
 
-export function param(data) {
-  let url = ''
-  for (let k in data) {
-    let value = data[k] !== undefined ? data[k] : ''
-    url += `&${k}=${encodeURIComponent(value)}`
+export function param (data) {
+  let url = '';
+  for (const k in data) {
+    let val = data[k] !== undefined ? data[k] : '';
+    url += `&${k}=${encodeURIComponent(val)}`
   }
-  return url ? url.substring(1) : ''
+  return url ? url.substring(1) : '';
 }
